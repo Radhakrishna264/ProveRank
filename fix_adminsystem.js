f1ac7bdb250c23e4d4de819a3f0527709bc5fbff
Radhakrishna264 <radhakrishnan100806@gmail.com>
2026-07-03 01:53:33 +0000

    cleanup: remove utility scripts from root

--- fix_adminsystem.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const fp = require('path').join(process.env.HOME, 'workspace/src/routes/adminSystem.js')
-let c = fs.readFileSync(fp, 'utf8')
-c = c.replace(
-  "router.get('/global-search-admin', verifyToken, isAdmin,",
-  "router.get('/global-search-admin', verifyToken, isSuperAdmin,"
-)
-fs.writeFileSync(fp, c)
-console.log('✅ Fixed')
--- fix_exam_attempts.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-  const result = await mongoose.connection.collection('exams').updateMany(
-    {},
-    { $set: { maxAttempts: 5, reAttemptLimit: 5 } }
-  );
-  console.log('Updated exams:', result.modifiedCount);
-  mongoose.disconnect();
-});
